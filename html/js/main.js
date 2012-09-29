ed1a32d84eda537e2acbf4a09e544de66348cf45
Rowell Cruz <r@rowellcruz.com>
2012-09-29 16:03:23 -0400

    Added card selector container and click handler

--- html/js/main.js
+++ html/js/main.js
@@ -7,11 +7,30 @@ function createTile() {
 
     image.src = "http://blog.catan.com/wp-content/uploads/2010/09/Floezbau.jpg"
     tile.appendChild(image);
+    tile.onclick = showCardSelector; 
     return tile;
 }
 
 function placeTileInto(rowElement, index) {
     rowElement.appendChild(createTile());
 
+}
+
+function init() {
+    var i;
+    for (i = 0; i < 5; i++) {
+        placeTileInto(topRow);
+        placeTileInto(cityRow);
+        placeTileInto(bottomRow);
+    }
+    rightContainer.onclick = hideCardSelector;
+}
+
+function showCardSelector() {
+    rightContainer.style.right = "0px";
+}
+
+function hideCardSelector() {
+    rightContainer.style.right = "-400px";
 }
 
